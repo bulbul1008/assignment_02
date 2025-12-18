e480dfdd9448da79ab2084a9baa853277241dd55
Shahaiar Khaled <mdkhaled199913@gmail.com>
2025-12-18 12:58:54 +0600

    check the branch

--- lib/Pages/custom_card.dart
+++ lib/Pages/custom_card.dart
@@ -11,80 +11,77 @@ class LayoutPractice extends StatelessWidget {
        crossAxisAlignment: CrossAxisAlignment.start,
 
         children: [
-          SingleChildScrollView(
-            child: Container(
-             width: 260,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(15),
-                    // alignment: Alignment.center,
-                    child: const Text(
-                      "Strawberry Pavlova",
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          Container(
+           width: 260,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(15),
+                  // alignment: Alignment.center,
+                  child: const Text(
+                    "Strawberry Pavlova",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Text(
+                  "Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. "
+                  "Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.",
+                  textAlign:TextAlign.center,
+                  style: TextStyle(fontSize: 14),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RatingStarGroup(rating: 3,),
+                    const Text(
+                      "170 Reviews",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                  const Text(
-                    "Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. "
-                    "Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.",
-                    textAlign:TextAlign.center,
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      RatingStarGroup(rating: 3,),
-                      const Text(
-                        "170 Reviews",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.ad_units, color: Colors.green),
+                        Text(
+                          "PREP:",
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Icon(Icons.ad_units, color: Colors.green),
-                          Text(
-                            "PREP:",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text("25 min"),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.punch_clock, color: Colors.green),
-                          Text(
-                            "COOk:",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('1 hr'),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Icon(Icons.kitchen, color: Colors.green),
-                          Text('FEEDS:'),
-                          Text('4-6'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                        Text("25 min"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.punch_clock, color: Colors.green),
+                        Text(
+                          "COOk:",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text('1 hr'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.kitchen, color: Colors.green),
+                        Text('FEEDS:'),
+                        Text('4-6'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
 
-
-          Expanded(child: Image.asset('assets/images/img.png')),
+          Flexible(child: Image.asset('assets/images/img.png')),
         ],
       ),
     );
